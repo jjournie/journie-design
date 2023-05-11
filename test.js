a545732136e8ded46e89f8dfe8fd72c0545d4469
Journie <83199081+jjournie@users.noreply.github.com>
2023-05-11 14:58:22 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export function TestComponent() {
-  return React.createElement("div", {}, "여러분 안녕하세요! 프레이머 스터디 함께 해주셔서 감사해요")
+  return React.createElement("div", {}, "여러분 모두 안녕하세요! 프레이머 스터디 함께 해주셔서 감사해요")
 }
